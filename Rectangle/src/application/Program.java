5c7a39167c66f637654884bb9dd505c5b9609e4c
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-02-26 20:23:29 -0300

    Rectangle

---
+++ Rectangle/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+import rectangle.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main (String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = input.nextDouble();
+        rectangle.height = input.nextDouble();
+
+        System.out.println(rectangle);
+
+        input.close();
+    }
+}
