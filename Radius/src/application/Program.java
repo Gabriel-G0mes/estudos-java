26326c77aa9a33c8145d4eb53e411a6f5f825e0a
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-03-06 05:34:26 -0300

    Radius Calc

---
+++ Radius/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+import util.Calculator;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        System.out.print("Enter Radius: ");
+        double radius = input.nextDouble();
+
+        double c = calc.circumference(radius);
+
+        double v = calc.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", calc.PI);
+        input.close();
+    }
+}
