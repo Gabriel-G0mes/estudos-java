08acf134f5cfaf11153c46f0007d56ffbeded049
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-03-11 21:29:53 -0300

    att code Estoque

--- Estoque/src/application/Program.java
+++ Estoque/src/application/Program.java
@@ -8,21 +8,21 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = input.nextLine();
+        String name = input.nextLine();
         System.out.print("Price: ");
-        product.price = input.nextDouble();
+        double price = input.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = input.nextInt();
+        int quantity = input.nextInt();
+        Product product = new Product(name, price, quantity);
 
         System.out.print("");
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = input.nextInt();
+        quantity = input.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
