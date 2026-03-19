39cd4b2c48ba89bd8e65d57ec579b14f66cff7b8
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-03-18 21:25:50 -0300

    new

--- Estoque/src/application/Program.java
+++ Estoque/src/application/Program.java
@@ -13,16 +13,14 @@ public class Program {
         String name = input.nextLine();
         System.out.print("Price: ");
         double price = input.nextDouble();
-        System.out.print("Quantity in stock: ");
-        int quantity = input.nextInt();
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.print("");
         System.out.println("Product data: " + product);
 
         System.out.println("");
         System.out.print("Enter the number of products to be added in stock: ");
-        quantity = input.nextInt();
+        int quantity = input.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
